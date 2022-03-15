969d78f4e8bdda11f70f89d4787dbbe038b75ece
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-03-15 16:25:14 -0300

    Pastas

---
+++ Front-end/src/test/java/com/example/tcc/Configuracao/TccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tcc.Configuracao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TccApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
