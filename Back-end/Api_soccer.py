2c0ea214ef3928f20820100116dc440d32fcdabb
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-03-30 16:48:07 -0300

    Api greenzord post ok

--- Back-end/Api_soccer.py
+++ Back-end/Api_soccer.py
@@ -1,10 +1,66 @@
 from flask import Flask, request
 from flask_restful import Resource,Api, abort, reqparse
 import Banco_de_dados
-from flask_sqlalchemy import SQLAlchemy
+from pydantic import BaseModel
 app = Flask(__name__)
 api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Greenzord.db'
+
+class campeonato_modelo(BaseModel):
+    nome: str
+
+class times_modelo(BaseModel):
+    nome: str
+
+class jogos_modelo(BaseModel):
+    campeonato: str
+    id: str
+    time_casa:str
+    resultado_casa: int
+    time_fora: str
+    resultado_fora: int
+    date: str
+    odd_casa: float
+    odd_fora: float
+    odd_empate: float
+
+class usuario_modelo(BaseModel):
+    username: str
+    nome: str
+    email: str
+    data_nascimento: str
+    saldo: float
+
+class bots_modelo(BaseModel):
+    nome: str
+    responsabilidade: float
+    odd_minima: float
+    odd_maxima: float
+    tempo_jogo_max: int
+    tempo_jogo_min: int
+    finalizacao_casa: int
+    finalizacao_fora: int
+    posse_bola_casa: int
+    posse_bola_fora: int
+    ativado: bool
+    casa_ou_fora: str
+    username: str
+
+
+class apostas_modelo(BaseModel):
+    mercado:str
+    valor_apostado: float
+    odd_aposta: float
+    id_bot:str
+    id_jogo: str
+
+class relatorio_modelo(BaseModel):
+    greens: int
+    reds: int
+    lucro: int
+    total_apostas: int
+    id_bot: str
+
 
 class Greenzord(Resource):
     def get(self, tipo, id):
@@ -57,17 +113,17 @@ class Greenzord(Resource):
 
         else:
             return {"Opcao invalida"}
-    def post(self, tipo, id):
-        if tipo == 1:
+     # def put(self, tipo, id):
+        # if tipo == 1:
 
-        elif tipo == 2:
-        elif tipo == 3:
-        elif tipo == 4:
-        elif tipo == 5:
-        elif tipo == 6:
-        elif tipo == 7:
-        else:
-            return {"Opcao invalida"}
+        # elif tipo == 2:
+        # elif tipo == 3:
+        # elif tipo == 4:
+        # elif tipo == 5:
+        # elif tipo == 6:
+        # elif tipo == 7:
+        # else:
+        #     return {"Opcao invalida"}
     def delete(self, tipo, id):
         if tipo == 1:
             convercao = str(id)
@@ -106,8 +162,44 @@ class Greenzord(Resource):
             return {200}
         else:
             return {"Opcao invalida"}
+class Greenzord_campeonato(Resource):
+    def post(campeonato: campeonato_modelo):
+        Banco_de_dados.add_campeonato(campeonato['nome'] )
+        return {"Campeonato:"+campeonato['nome'] +"Cadastrado com sucesso"}
 
+class Greenzord_times(Resource):
+    def post(time:times_modelo):
+        Banco_de_dados.add_times(time['nome'])
+        return {"Time:"+time['nome'] + "Cadastrado com sucesso"}
 
+class Greenzord_jogos(Resource):
+    def post(jogos: jogos_modelo):
+        Banco_de_dados.add_jogos(jogos['campeonato'],jogos['id'],jogos['time_casa'],jogos['resultado_casa'],jogos['resultado_fora'],jogos['data'],jogos['odd_casa'],jogos['odd_fora'],jogos['odd_empate'])
+        return {"Jogo:"+jogos['id'] + "Cadastrado com sucesso"}
+class Greenzord_usuario(Resource):
+    def post(usuario: usuario_modelo):
+        Banco_de_dados.add_usuario(usuario['username'],usuario['nome'],usuario['email'],usuario['data_nascimento'],usuario['saldo'])
+        return {"Usuario:"+usuario['nome'+"cadastrado com sucesso"]}
+class Greenzord_bots(Resource):
+    def post(bots: bots_modelo):
+        Banco_de_dados.add_bots(bots['nome'],bots['responsabilidade'],bots['odd_minima'],bots['odd_maxima'],bots['tempo_jogo_min'],bots['tempo_jogo_max'],bots['finalizacao_casa'],bots['finalizacao_fora'],bots['posse_bola_casa'],
+                                bots['posse_bola_fora'],bots['ativado'],bots['casa_ou_fora'],bots['username'])
+        return {"Bot:"+bots['nome']+"cadastrado com sucesso"}
+class Greenzord_apostas(Resource):
+    def post(aposta: apostas_modelo):
+        Banco_de_dados.add_apostas(aposta['mercado'],aposta['valor_apostado'],aposta['odd_aposta'],aposta['id_bot'],aposta['id_jogo'])
+        return {"Aposta realizada com sucesso!"}
+class Greenzord_relatorio(Resource):
+    def post(relatorio: relatorio_modelo):
+        Banco_de_dados.add_relatorio(relatorio['greens'],relatorio['lucro'],relatorio['total_apostas'],relatorio['id_bot'])
+        return {"Relatorio cadastrado com sucesso"}
 api.add_resource(Greenzord,"/greenzord/<int:tipo>/<int:id>")
+api.add_resource(Greenzord_campeonato,"/greenzord/campeonato")
+api.add_resource(Greenzord_times,"/greenzord/times")
+api.add_resource(Greenzord_jogos,"/greenzord/jogos")
+api.add_resource(Greenzord_usuario,"/greenzord/usuario")
+api.add_resource(Greenzord_bots,"/greenzord/bots")
+api.add_resource(Greenzord_apostas,"/greenzord/apostas")
+api.add_resource(Greenzord_relatorio,"/greenzord/relatorio")
 if __name__=="__main__":
     app.run(debug= True)
