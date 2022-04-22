ad7ad09e1e64347935d527d1a79cb6a20b5fda9b
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-04-22 16:12:35 -0300

    Apagando as funções que não usa

--- Back-end/Api_soccer.py
+++ Back-end/Api_soccer.py
@@ -12,14 +12,6 @@ api = Api(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Greenzord.db'
 
 
-class campeonato_modelo(BaseModel):
-    nome: str
-
-
-class times_modelo(BaseModel):
-    nome: str
-
-
 class greenzord_login(BaseModel):
     username: str
     senha: str
@@ -183,82 +175,10 @@ class Greenzord(Resource):
         else:
             return {"Opcao invalida"}
 
-    # def put(self, tipo, id):
-    #     if tipo == 1:
-    #
-    #     elif tipo == 2:
-    #     elif tipo == 3:
-    #     elif tipo == 4:
-    #     elif tipo == 5:
-    #     elif tipo == 6:
-    #     elif tipo == 7:
-    #     else:
-    # return {"Opcao invalida"}
-
-    def delete(self, tipo, id):
-        if tipo == 1:
-            convercao = str(id)
-            acao = "DELETE FROM Campeonato WHERE id_campeonato =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 2:
-            convercao = str(id)
-            acao = "DELETE FROM Times WHERE id_time =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 3:
-            convercao = str(id)
-            acao = "DELETE FROM Jogos WHERE id_jogo =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 4:
-            convercao = str(id)
-            acao = "DELETE FROM Usuario WHERE id_usuario =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 5:
-            convercao = str(id)
-            acao = "DELETE FROM Bots WHERE id_bot =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 6:
-            convercao = str(id)
-            acao = "DELETE FROM Apostas WHERE id_apostas =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        elif tipo == 7:
-            convercao = str(id)
-            acao = "DELETE FROM Relatorio WHERE id_relatorio =" + convercao
-            Banco_de_dados.deletar(acao)
-            return 200
-        else:
-            return {"Opcao invalida"}
-
-
-class Greenzord_campeonato(Resource):
-    def post(campeonato: campeonato_modelo):
-        Banco_de_dados.add_campeonato(request.json['nome'])
-        return 200
-
-
-class Greenzord_times(Resource):
-    def post(time: times_modelo):
-        Banco_de_dados.add_times(request.json['nome'])
-        return 200
-
 
 class Greenzord_jogos(Resource):
     def get(self):
         Web_scraping.WebScraping()
-
-
-class Greenzord_usuario(Resource):
-    def post(usuario: usuario_modelo):
-        Banco_de_dados.add_usuario(request.json['username'], request.json['nome'], request.json['email'],
-                                   request.json['data_nascimento'],
-                                   float(request.json['saldo']))
-        return 200
-
 
 class Greenzord_bots(Resource):
     def post(bots: bots_modelo):
@@ -290,13 +210,6 @@ class Greenzord_bots_editar(Resource):
         Banco_de_dados.atulizar_bots(bool(request.json['ativado']), id, 11)
         Banco_de_dados.atulizar_bots(request.json['apostar'], id, 12)
         Banco_de_dados.atulizar_bots(request.json['analisar'], id, 13)
-        return 200
-
-
-class Greenzord_apostas(Resource):
-    def post(aposta: apostas_modelo):
-        Banco_de_dados.add_apostas(request.json['mercado'], float(request.json['valor_apostado']),
-                                   float(request.json['odd_aposta']), request.json['id_bot'], request.json['id_jogo'])
         return 200
 
 
@@ -347,12 +260,8 @@ class Greenzord_relatorio(Resource):
 
 
 api.add_resource(Greenzord, "/greenzord/<int:tipo>/<int:tipo2>/<string:nome>")
-api.add_resource(Greenzord_campeonato, "/greenzord/campeonato")
-api.add_resource(Greenzord_times, "/greenzord/times")
 api.add_resource(Greenzord_jogos, "/greenzord/jogos")
-api.add_resource(Greenzord_usuario, "/greenzord/usuario")
 api.add_resource(Greenzord_bots, "/greenzord/bots")
-api.add_resource(Greenzord_apostas, "/greenzord/apostas")
 api.add_resource(Greenzord_apagar_bots, "/greenzord/bots/deletar/<int:id>")
 api.add_resource(Greenzord_bots_editar, "/greenzord/bots/editar/<int:id>")
 api.add_resource(Greenzord_Login_Cadastro, "/greenzord/login_cadastro")
