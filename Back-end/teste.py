ec54c7940dd3e0da36aa616783a182e7db835469
Jotage777 <gabrieloliveira_pb@hotmail.com>
2022-05-05 11:54:26 -0300

    S[o apostar quando o jogo estiver empate

--- Back-end/teste.py
+++ Back-end/teste.py
@@ -2,6 +2,6 @@ import Web_scraping
 import Apostar
 import Banco_de_dados
 
-Web_scraping.WebScraping()
-# Apostar.analise()
+#Web_scraping.WebScraping()
+Apostar.analise()
 # Banco_de_dados.atualizar_jogos('fechado', 'z7h4xEia', 11)
