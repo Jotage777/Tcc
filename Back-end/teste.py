3a70dd9c5a949c648d0bc9e517f9fd070ccdde3c
Messias443 <joaomessias443@gmail.com>
2022-05-06 10:44:57 -0300

    Reorganizando Apostar.py

--- Back-end/teste.py
+++ Back-end/teste.py
@@ -2,7 +2,7 @@ import Web_scraping
 import Apostar
 import Banco_de_dados
 
-#Web_scraping.WebScraping()
+# Web_scraping.WebScraping()
 # Apostar.analise()
 # Banco_de_dados.atualizar_jogos('fechado', 'z7h4xEia', 11)
-Banco_de_dados.atulizar_bots(0, 1, 11)
+# Banco_de_dados.atulizar_bots(0, 1, 11)
